55cfce4da0e5614730d51ef33c2cc03e03d9e1a1
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-22 11:30:07 -0300

    Adding bindings with findViewById

--- app/src/main/java/com/example/findviewbyid/MainActivity.kt
+++ app/src/main/java/com/example/findviewbyid/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.findviewbyid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logoImage = findViewById<ImageView>(R.id.androidLogo)
+
+        val nameText = findViewById<TextView>(R.id.name)
+        val nicknameText = findViewById<TextView>(R.id.nickname)
+        val countryText = findViewById<TextView>(R.id.country)
+
+        val styleOneText = findViewById<TextView>(R.id.style_one)
+        val styleTwoText = findViewById<TextView>(R.id.style_two)
+        val styleThreeText = findViewById<TextView>(R.id.style_three)
+        val styleFourText = findViewById<TextView>(R.id.style_four)
+
+        val clickmeButton = findViewById<Button>(R.id.clickmeButton)
+
+        logoImage.setBackgroundResource(R.drawable.android)
+
+        nameText.text = "Felix Arvid Ulf Kjellberg"
+        nicknameText.text = "PewDiePie"
+        countryText.text = "Sweden"
+
+        styleOneText.text = "Games"
+        styleTwoText.text = "Comedy"
+        styleThreeText.text = "React"
+        styleFourText.text = "Terror"
+
+        clickmeButton.setOnClickListener {
+            Log.e("TAG", "Hello!")
+        }
     }
 }
